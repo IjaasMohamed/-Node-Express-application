6cf478efaaa6f5dc0bd5085a172b9f2696ef5cf6
IjaasMohamed <undyin0@gmail.com>
2023-03-07 02:36:04 -0300

    dotenv error spotted

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
 const app = express();
 
-// dotenv.config({ path: 'config.env' });
-const PORT = process.env.PORT || 5000; // store in env file // if the value is not available, then use the default port
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080; // store in env file // if the value is not available, then use the default port
 
 app.get('/', (req, res) => {
   res.send('Node Express Application');
