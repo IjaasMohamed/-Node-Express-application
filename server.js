b3ea5d71800203057349056de11f11278a3025ae
IjaasMohamed <undyin0@gmail.com>
2023-03-07 02:18:40 -0300

    inital commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// const dotenv = require('dotenv');
+
+const app = express();
+
+// dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 5000; // store in env file // if the value is not available, then use the default port
+
+app.get('/', (req, res) => {
+  res.send('Node Express Application');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
